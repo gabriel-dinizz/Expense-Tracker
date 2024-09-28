e816c6d854f3928fd67f29e17d779f8d295a9e2c
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-09-28 09:55:24 -0300

    Refactor app.js to connect to the database and sync models

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const sequelize = require("./config/database");
+const userRoutes = require("./routes/userRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json()); // Parse JSON body
+app.use(userRoutes); // Load user routes
+app.use(expenseRoutes); // Load expense routes
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the expense tracker");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected...");
+    return sequelize.sync(); // Sync all models
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => console.error("Error connecting to the database:", err));
